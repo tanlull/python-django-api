5c0c80e7e503a8753197c96af1c654f42d9bc76d
Tanya Sattaya-aphitan <tanlull@gmail.com>
2020-10-05 01:02:15 +0700

    OK

---
+++ PythonDjango/countries/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from countries.models import Countries
+
+class CountriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Countries
+        fields = ('id','name','capital')
